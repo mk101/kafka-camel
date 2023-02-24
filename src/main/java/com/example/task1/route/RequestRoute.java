82317d506138b8926f0755602edf913c7ec247df
Maxim Kolesov <pro100maks222@gmail.com>
2023-02-24 10:32:12 +0300

    Connect kafka and camel

--- src/main/java/com/example/task1/route/RequestRoute.java
+++ src/main/java/com/example/task1/route/RequestRoute.java
@@ -1,7 +1,7 @@
 package com.example.task1.route;
 
-import com.example.task1.generated.User;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.kafka.KafkaConstants;
 import org.apache.camel.converter.jaxb.JaxbDataFormat;
 import org.apache.camel.spi.DataFormat;
 import org.springframework.stereotype.Component;
@@ -12,8 +12,16 @@ public class RequestRoute extends RouteBuilder {
     public void configure() throws Exception {
         DataFormat jaxb = new JaxbDataFormat("com.example.task1.generated");
 
-        from("file://C:/Projects/java/globus/task1/src/main/resources/test-data")
-                .unmarshal(jaxb)
-                .log("${body}");
+        from("file:C:\\Projects\\java\\globus\\task1\\src\\main\\resources\\test-data")
+                .setHeader(KafkaConstants.KEY, constant("Camel"))
+                        .to("kafka:requests?brokers={{kafka.broker1.host}}");
+
+        from("kafka:requests?brokers={{kafka.broker1.host}}&groupId=camel")
+                .log("Message received from Kafka : ${body}")
+                .log("    on the topic ${headers[kafka.TOPIC]}")
+                .log("    on the partition ${headers[kafka.PARTITION]}")
+                .log("    with the offset ${headers[kafka.OFFSET]}");
+
+        jaxb.close();
     }
 }
